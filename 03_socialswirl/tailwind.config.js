395d2a052a8d974e77356d228550fc1dbda889a5
TurabAli004 <turabalidev004@gmail.com>
2024-08-06 22:14:25 +0500

    social swirl three days work

---
+++ 03_socialswirl/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
